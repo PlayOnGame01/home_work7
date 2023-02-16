e685ad9f2028b88247379fc17ecdb7ff0c65e796
Artem <artemzhdanov1299@gmail.com>
2023-02-16 22:18:30 +0200

    Add project files.

---
+++ home_work7/Matrix.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace _.NET_homework_10._02
+{
+    internal class Matrix
+    {
+        private int[,] mas = new int[3, 3];
+        public Matrix() { }
+        public Matrix(int[,] mas)
+        {
+            this.mas = mas;
+        }
+        public void Enter()
+        {
+            WriteLine("Enter num for matrix:");
+            string num = null;
+            for (int i = 0; i < mas.GetLength(0); i++)
+            {
+                for (int j = 0; j < mas.GetLength(1); j++)
+                {
+                    num = Console.ReadLine();
+                    mas[i, j] = Convert.ToInt32(num);
+                }
+            }
+            WriteLine();
+        }
+        public void Print()
+        {
+            for (int i = 0; i < mas.GetLength(0); i++)
+            {
+                for (int j = 0; j < mas.GetLength(1); j++)
+                {
+                    Write($"{mas[i, j]}  ");
+                }
+                WriteLine();
+            }
+            WriteLine();
+        }
+        public void MaxMinElement()
+        {
+            int max = 0, min = mas[0, 0];
+            for (int i = 0; i < mas.GetLength(0); i++)
+            {
+                for (int j = 0; j < mas.GetLength(1); j++)
+                {
+                    if (max < mas[i, j]) { max = mas[i, j]; }
+                    if (min > mas[i, j]) { min = mas[i, j]; }
+                }
+            }
+            WriteLine($"Max element in matrix: {max}\nMin element in matrix: {min}");
+        }
+        public static Matrix operator +(Matrix obj1, Matrix obj2)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    obj1.mas[i, j] += obj2.mas[i, j];
+                }
+            }
+            return obj1;
+        }
+        public static Matrix operator -(Matrix obj1, Matrix obj2)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    obj1.mas[i, j] -= obj2.mas[i, j];
+                }
+            }
+            return obj1;
+        }
+        public static Matrix operator *(Matrix obj1, Matrix obj2)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    obj1.mas[i, j] *= obj2.mas[i, j];
+                }
+            }
+            return obj1;
+        }
+        public static Matrix operator *(Matrix obj, int a)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    obj.mas[i, j] *= a;
+                }
+            }
+            return obj;
+        }
+        public static bool operator ==(Matrix obj1, Matrix obj2)
+        {
+            int count = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (obj1.mas[i, j] == obj2.mas[i, j]) { count++; }
+                }
+            }
+            if (count == 9)
+                return true;
+            else
+                return false;
+        }
+        public static bool operator !=(Matrix obj1, Matrix obj2)
+        {
+            int count = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (obj1.mas[i, j] != obj2.mas[i, j]) { i = 3; j = 3; count = 10; }
+                }
+            }
+            if (count == 10)
+                return true;
+            else
+                return false;
+        }
+        public int this[int i, int j]
+        {
+            get => mas[i, j];
+            set => mas[i, j] = value;
+        }
+    }
+}
---
+++ home_work7/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Перегрузка_операции_homework_10._02._2023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Matrix matrix = new Matrix();
+            //matrix.Enter();
+            //matrix.Print();
+            //Matrix matrix2 = new Matrix();
+            //matrix2.Enter();
+            //matrix2.Print();
+            //matrix *= 2;
+            //matrix.Print();
+            //if (matrix != matrix2) { Console.WriteLine("matrix != matrix2"); }
+            //else { Console.WriteLine("matrix == matrix2"); }
+            matrix[0, 0] = 100;
+            matrix[2, 2] = 200;
+            matrix.Print();
+        }
+    }
+}
